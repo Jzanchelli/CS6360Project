c4059988e4ac0e82f70bb7279e3f55f3cd5fc464
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-10-14 12:19:00 -0600

    added vibration and table
    added vibration to fire script and added table to make grabbing guns easier.

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -11,32 +11,49 @@ public class Fire : MonoBehaviour
     public AudioClip audio;
     public AudioSource audioSource;
     public OVRGrabbable gun;
-    //private OVRGrabbable gabbable;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip= audio;
+        this.audioSource.clip= audio;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (grabbable.isGrabbed())
-        //{
-            if(gun.isGrabbed && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, gun.grabbedBy.GetController()))
-            {
-                Triggered();
-            }
-       // }
+        if(this.gun.isGrabbed && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, this.gun.grabbedBy.GetController()))
+        {
+            Triggered(this.gun.grabbedBy.GetController());
+        }
     }
 
-    public void Triggered()
+    public void Triggered(OVRInput.Controller controller)
     {
         audioSource.Play();
         GameObject newBullet = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
         Rigidbody newBulletRigidbody = newBullet.GetComponent<Rigidbody>();
         newBulletRigidbody.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
         Destroy(newBullet, 4.0f);
+        byte[] noize = { 255 };
+        if (controller == OVRInput.Controller.LTouch)
+        {
+            //OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+            OVRHaptics.LeftChannel.Preempt(new OVRHapticsClip(noize, 1));
+        }
+        else if (controller == OVRInput.Controller.RTouch)
+        {
+            OVRHaptics.RightChannel.Preempt(new OVRHapticsClip(noize, 1));
+            //OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        }
+        /*yield return new WaitForSeconds(0.5f);
+        if (controller == OVRInput.Controller.LTouch)
+        {
+            OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+        }
+        else if (controller == OVRInput.Controller.RTouch)
+        {
+            OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+        }*/
+
     }
 }
