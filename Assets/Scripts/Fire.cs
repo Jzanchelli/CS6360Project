76081ca4dbd6af459654a9069747c0d4b44ef139
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-10-13 20:39:32 -0600

    added guns, grabbing capability, bullets, firing sounds
    added guns for both hands, (on a platform because sometimes the gun falls below the mesh and is unretreivable), added grabbing capability for each hand and adjusted hand position to appear as if guns are being held(trigger finger is weird and this hold could be improved), added bullets and bullet emitters. added script to shoot the gun. Reorganized models in asset folder

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject bulletEmitter;
+    public GameObject bullet;
+    public float bulletSpeed;
+    public AudioClip audio;
+    public AudioSource audioSource;
+    public OVRGrabbable gun;
+    //private OVRGrabbable gabbable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip= audio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (grabbable.isGrabbed())
+        //{
+            if(gun.isGrabbed && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, gun.grabbedBy.GetController()))
+            {
+                Triggered();
+            }
+       // }
+    }
+
+    public void Triggered()
+    {
+        audioSource.Play();
+        GameObject newBullet = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+        Rigidbody newBulletRigidbody = newBullet.GetComponent<Rigidbody>();
+        newBulletRigidbody.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+        Destroy(newBullet, 4.0f);
+    }
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Policy;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public OVRInput.Controller controller;
+    private OVRGrabber parentObject;
+    private GameObject gun;
+    //private GrabbableProperties properties;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //animation = GetComponent<ShootAnimation>();
+        parentObject = this.GetComponent<OVRGrabber>();
+        OVRGrabbable grabbed = parentObject.grabbedObject;
+        gun = grabbed.transform.parent.gameObject;
+        //properties = gun.GetComponent(typeof(GrabbableProperties)) as GrabbableProperties;
+        //properties = gun.GetComponent<GrabbablePropeties>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (properties.isGun && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, this.controller));
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, this.controller))
+        {
+            Fire fire = gun.GetComponent(typeof(Fire)) as Fire;
+            fire.Triggered();
+            UnityEngine.Debug.Log("isTriggered");
+            //GameOject newBullet = Instantiate(bullet, bulletEmitter.transform.position, gun.bulletEmitter.transform.rotation) as GameObject;
+            //Rigidbody newBulletRigidbody = newBullet.GetComponent<Rigidbody>();
+            //newBulletRigidbody.AddForce(transform.forward * bulletSpeed);
+            //Destroy(newBullet, 10.0f);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("is not Triggered");
+        }
+    }
+}
