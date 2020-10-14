687f5622e81849111d996fede8ffbae069e40bd0
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-10-13 22:00:49 -0600

    guns are firing
    shot gun, rifle and pistole all with firing sound and grip adjusted. would be nice to have a double handed hold on the rifle and shotgun but this hasn't been implemented.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.Debug.Log("hit " + other.name + "!");
+        Destroy(gameObject);
+    }
+}
