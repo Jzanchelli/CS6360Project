2fcadf88e5be952d7fb4087e3b7671a0048bbd51
Andrew Haas <andhaas1@gmail.com>
2020-11-02 15:24:57 -0700

    First version of horse control using hand movement. Grab ball and move any direction to move horse that direction

--- Assets/Scripts/HorseControls.cs
+++ Assets/Scripts/HorseControls.cs
@@ -10,16 +10,22 @@ public class HorseControls : MonoBehaviour
     public float speed = 1;
     private CharacterController characterController;
     private Coroutine rotateCoroutine;
+    private GameObject gameObj;
+    private Reins reins;
+    private Vector3 reinsDiff;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        gameObj = GameObject.FindGameObjectWithTag("reins");
+        reins = gameObj.GetComponent<Reins>();
     }
 
     // Update is called once per frame
     void Update()
-    {   
-            RotatePlayer(input.axis.x);              
+    {
+        reinsDiff = reins.getDiff();
+        RotatePlayer(reinsDiff.x *2);
     }
 
     public void RotatePlayer(float angle)
@@ -44,10 +50,10 @@ public class HorseControls : MonoBehaviour
         playerFeetOffset = Quaternion.Euler(0.0f, angle, 0.0f) * playerFeetOffset;
         player.trackingOriginTransform.position += playerFeetOffset;
 
-        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(0, 0, input.axis.y));
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(0, 0, reinsDiff.z *4));
         characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
 
-        return null;
+        yield return this;
 
     }
 
