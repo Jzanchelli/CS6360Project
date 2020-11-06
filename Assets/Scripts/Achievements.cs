a144ea83fb63483681870886b0980eae8bd3c14a
Michael <mab8192@gmail.com>
2020-11-06 09:41:00 -0700

    renamed to open world, create achievements enum

---
+++ Assets/Scripts/Achievements.cs
@@ -0,0 +1,13 @@
+﻿public enum Achievement
+{
+    hit_bottle_from_50,
+    travel_100,
+    hit_target_while_riding,
+    hit_target_25_while_riding,
+    get_to_second_town,
+    level_one_100_accuracy,
+    level_one_score_1000,
+    level_two_time_gold,
+    level_two_time_silver,
+    level_two_time_bronze
+}
