bb534bac4c24690778cbe7caeeeb65fdb8fcf44b
Hunter <u0916776@utah.edu>
2020-11-08 01:46:22 -0700

    Fixed Pause Menu
    Fixed pause menu loading multiple times. Various tweaks to try and get pause menu to perform better.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,67 +4,72 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
+using Valve.VR.Extras;
 
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject resumeTrigger;
-    public GameObject optionsTrigger;
-    public GameObject quitTrigger;
-    public GameObject menuTrigger;
+    // public GameObject resumeTrigger;
+    // public GameObject optionsTrigger;
+    // public GameObject quitTrigger;
+    // public GameObject menuTrigger;
     private int menuSceneIndex;
 
-    public static PauseMenu instance;
+    private GameObject rightHand;
+
+    private bool loaded;
     
-    
+    //public static SteamVR_LaserPointer rightPointer;
     void Start()
     {
+        loaded = false;
         //UnloadMenu();
         menuSceneIndex = 0;
-        
+        //rightHand = this.gameObject.transform.Find("RightHand").gameObject;
     }    
     
     // Update is called once per frame
     void Update()
     {
-        lock (instance)
-        {
                     //This works, for some reason using the same button registers it for 2 frames
             bool menuState = SteamVR_Input.GetState("LaunchMenu",SteamVR_Input_Sources.LeftHand);
+            //UnityEngine.Debug.Log(loaded);
             if(menuState)
             {
-                //UnityEngine.Debug.Log("Menu Pressed");
-            
+                //UnityEngine.Debug.Log("menustate = true");
+                if(!loaded)
+                {
                     LoadMenu();
-                    //Time.timeScale = 0;
+                    Time.timeScale = 0;
+                }
             }
             else
             {
                 //UnityEngine.Debug.Log("A Pressed");
-                if(SceneManager.GetSceneByBuildIndex(menuSceneIndex).isLoaded)
+                if(loaded)
                 {
+                    
                     UnloadMenu();
-                    //Time.timeScale = 1;
+                    Time.timeScale = 1;
                 }
             }        
-
-        }
         
     }
 
     public void LoadMenu()
     {
-        UnityEngine.Debug.Log("Trying to load menu");
-        if(!SceneManager.GetSceneByBuildIndex(menuSceneIndex).isLoaded)
-            SceneManager.LoadScene(menuSceneIndex, LoadSceneMode.Additive);
+        //UnityEngine.Debug.Log("Trying to load menu");        
+        SceneManager.LoadSceneAsync(menuSceneIndex, LoadSceneMode.Additive);
+        loaded = true;
     }
 
     public void UnloadMenu()
     {
-        UnityEngine.Debug.Log("Trying to unload menu");
+        //UnityEngine.Debug.Log("Trying to unload menu");
         SceneManager.UnloadSceneAsync(menuSceneIndex);
         Resources.UnloadAsset(this);
+        loaded = false;
         //Resources.UnloadAsset(optionsTrigger);
         //Resources.UnloadAsset(quitTrigger);
         //Resources.UnloadAsset(menuTrigger);
