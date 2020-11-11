5faa61f3f788a9fb6065842dddcecde487ff8347
Lucas Idstrom <31257219+u0571702@users.noreply.github.com>
2020-11-11 00:32:15 -0700

    Init Commit

---
+++ Assets/item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [SerializeField]
+    GameObject controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 50 * Time.deltaTime, 0);
+        controller.GetComponent<Controller>.
+    }
+}
