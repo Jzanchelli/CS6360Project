085ffd0087142df71a315c178585560cfc1b841f
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-11-25 08:36:30 -0700

    completed sniper rifle prefab
    finished scope for sniper rifle and tested long distance shooting

---
+++ Assets/Scripts/TargetScripts/BaisWhiskeyBottleTargetAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaisWhiskeyBottleTargetAction : MonoBehaviour, TargetAction
+{
+    public AudioClip glassShatterClip;
+    public AudioSource audioSource;
+    //public Level4GameController gameController;
+
+    public bool PerformAction()
+    {
+        //UnityEngine.Debug.Log("performing target action");
+        //audioSource.clip = this.glassShatterClip;
+       // audioSource.Play();
+       // StartCoroutine("WaitToMoveOn");
+        //gameController.StartLevel();
+        //playAudio();
+        //while (audioSource.isPlaying){}
+        //gameController.StartLevel();
+        return true;
+    }
+
+    IEnumerator WaitToMoveOn()
+    {
+        // audioSource.clip = this.glassShatterClip;
+        //audioSource.Play();
+        yield return new WaitForSeconds(2);
+    }
+}
