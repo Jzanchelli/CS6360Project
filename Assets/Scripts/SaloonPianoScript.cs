065d346e4c19da43aedef422d4f158ff4dc0eeb0
tjensen123 <31261947+tjensen123@users.noreply.github.com>
2020-12-08 12:11:00 -0700

    added script to piano to choose songs randomly
    added a piano script to choose songs randomly

---
+++ Assets/Scripts/SaloonPianoScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaloonPianoScript : MonoBehaviour
+{
+    public AudioClip[] saloonSongs;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audioSource.clip = saloonSongs[Random.Range(0, saloonSongs.Length)%3];
+        this.audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
